59b463924123ba8c4856eae3eccb5d3d24b4c0d4
JonathanL <jonathanledezma09@gmail.com>
2023-07-17 20:10:27 -0300

    Mensaje acomodando deploy 5

--- api/src/db.js
+++ api/src/db.js
@@ -1,16 +1,29 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// const fs = require("fs");
+// const path = require("path");
+// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
